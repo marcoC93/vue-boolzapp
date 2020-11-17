511e1c47c6b99077ff4ea46fd790a664baf3e0bd
Marco <marco.chianese93@gmail.com>
2020-11-18 00:40:44 +0100

    Inserimento commenti vari ( da terminare)

--- script/script.js
+++ script/script.js
@@ -1,12 +1,17 @@
 var app = new Vue({
   el: '#root',
   data: {
-
+    // qui "dichiaro le diverse 'variabili' che immagazzineranno i diversi input ricevuti dall'HTML  "
+        // questo é l'indice dei contatti per fare in modo che l'array di contatti venga stampato dinamicamente nell'apposita sezione all'interno dell'html
       contactIndex: 0,
+      // é qui verrá immagazzinato l'input html nella sezione chat e verrá aggiunto all'array di messaggi relativo alla chat attiva
       message_sent: '' ,
+      // questa propietá é stata creata per far in modo che lo scope della risposta automatica sia visibile anche all'esterno della funzione
       message_received: '',
+      //qui c'é l'input del campo di ricerca il quale va immagazzinato per essere processato nell'apposita funzione
       contact_search: '',
 
+    // Questo é l'array dei contatti ed in ogni contatto é presente anche lo scambio di messaggi rappresentato  da un array di oggetti con diverse info su ogni messaggio
       contacts: [
              {
                       name: 'Michele',
@@ -94,13 +99,13 @@ var app = new Vue({
     ],
 
   },
-
+    // qui sono presenti diverse funzioni atte a processare i vari input che l'utente immetterá in pagina
      methods : {
-
+        //in questa funzione tramite il cambio dell'indice assegnando l'indice corrente con l'indice dell'elemento cliccato permette di switchare da una chat all'altra
         change_current_contact(contactIndex) {
             this.contactIndex = contactIndex;
         },
-
+        // in questa funzione tramite l'input ricevuto in html ed aggiungendolo all'array messaggi del contatto corrente
         send_message(message_sent) {
             this.contacts[this.contactIndex].messages.push(
                 {
@@ -126,10 +131,13 @@ var app = new Vue({
            this.contacts.forEach((contact,index, contacs ) => {
 
                // se il nome del contatto attuale è filtrato da contact_search
+               // in questa variabile é presente l'input di ricerca contatti dell'utente
                const search_string = this.contact_search ;
+               // qui l'input dell'utente 'e stato modificato in modo da rendere mauscolo li primo carattere con il metodo .charAt(indice 0 in quanto di tratta della prima lettera) ed unito con la il resto della stringa prendendola completa ma rimuovendo il primo carattere in stringa il quale sarebbe minuscolo
                const search_string_capitalized = search_string.charAt(0).toUpperCase() + search_string.slice(1) ;
-               console.log(search_string);
-               console.log(search_string_capitalized);
+                // DEBUG:
+               // console.log(search_string);
+               // console.log(search_string_capitalized);
                 if (contact.name.includes(search_string_capitalized)) {
 
                     // setta il contatto a visible = true
